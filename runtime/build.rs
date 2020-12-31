3be0d75b59773e38b5b587867c540db61fcb2f66
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2020-12-31 16:24:19 +0530

    wasm 3.0

--- runtime/build.rs
+++ runtime/build.rs
@@ -4,12 +4,11 @@
  *
  */
 
-use wasm_builder_runner::WasmBuilder;
+use wasm_builder::WasmBuilder;
 
 fn main() {
 	WasmBuilder::new()
 		.with_current_project()
-		.with_wasm_builder_from_crates("2.0.0")
 		.export_heap_base()
 		.import_memory()
 		.build()
