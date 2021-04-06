370ba3e8d2d6a3b999e2b098ce1510c5e50e12a2
Pranshu <pranshurastogi3196@gmail.com>
2021-04-06 16:25:47 +0530

    WIP: Mtype benchmarking added

---
+++ pallets/mtype/src/benchmarking.rs
@@ -0,0 +1,39 @@
+// Copyright 2019-2021 Dhiway.
+// This file is part of CORD Platform.
+
+//! Benchmarking of Mtype
+
+
+#![cfg(feature = "runtime-benchmarks")]
+
+use super::*;
+
+use frame_benchmarking::{account, benchmarks};
+use frame_system::RawOrigin;
+
+const SEED: u32 = 0;
+
+benchmarks! {
+	add {
+		let caller = account("caller", 0, SEED);
+		let hash = <T::Hash as Default>::default();
+
+	}: _(RawOrigin::Signed(caller), hash)
+	verify {
+		MTYPEs::<T>::contains_key(hash)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::tests::{new_test_ext, Test};
+	use frame_support::assert_ok;
+
+	#[test]
+	fn test_benchmarks() {
+		new_test_ext().execute_with(|| {
+			assert_ok!(test_benchmark_add::<Test>());
+		});
+	}
+}
