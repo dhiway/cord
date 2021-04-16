78a3f74736b8434922798e8f6431b84c65a4fb7e
Pranshu <pranshurastogi3196@gmail.com>
2021-04-16 08:35:31 +0530

    mtype benchmarking done

--- pallets/mtype/src/benchmarking.rs
+++ pallets/mtype/src/benchmarking.rs
@@ -15,7 +15,7 @@ use sp_std::{vec, vec::Vec, boxed::Box};
 const SEED: u32 = 0;
 
 benchmarks! {
-	add {
+	anchor {
 		let caller = account("caller", 0, SEED);
 		let hash = <T::Hash as Default>::default();
 
