25a7d7374ea3f709149e02e0fd123df03491a8e1
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2022-02-24 02:08:44 +0530

    0.6.x content identifier with prefix

--- pallets/schema/src/lib.rs
+++ pallets/schema/src/lib.rs
@@ -17,7 +17,7 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(clippy::unused_unit)]
-pub use cord_primitives::{CidOf, StatusOf, VersionOf};
+pub use cord_primitives::{CidOf, IdentifierOf, StatusOf, VersionOf};
 use frame_support::{ensure, storage::types::StorageMap, BoundedVec};
 use semver::Version;
 use sp_std::{fmt::Debug, prelude::Clone, str, vec::Vec};
@@ -36,16 +36,12 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
-	/// Identifier of a schema.
-	pub type IdOf<T> = <T as frame_system::Config>::Hash;
-	/// IPFS CID of a schema.
-	// pub type VidOf<T> = BoundedVec<u8, <T as Config>::MaxLength>;
 	/// Hash of the schema.
 	pub type HashOf<T> = <T as frame_system::Config>::Hash;
 	/// Type of a CORD account.
 	pub type CordAccountOf<T> = <T as Config>::CordAccountId;
-	/// Type for a block number.
-	pub type BlockNumberOf<T> = <T as frame_system::Config>::BlockNumber;
+	// schema identifier prefix.
+	pub const SCHEMA_IDENTIFIER_PREFIX: u16 = 1041;
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -78,7 +74,7 @@ pub mod pallet {
 	/// It maps from a schema identifier to hash.
 	#[pallet::storage]
 	#[pallet::getter(fn schemaid)]
-	pub type SchemaId<T> = StorageMap<_, Blake2_128Concat, IdOf<T>, HashOf<T>>;
+	pub type SchemaId<T> = StorageMap<_, Blake2_128Concat, IdentifierOf, HashOf<T>>;
 
 	/// schema delegations stored on chain.
 	/// It maps from an identifier to a vector of delegates.
@@ -87,7 +83,7 @@ pub mod pallet {
 	pub(super) type Delegations<T: Config> = StorageMap<
 		_,
 		Blake2_128Concat,
-		IdOf<T>,
+		IdentifierOf,
 		BoundedVec<CordAccountOf<T>, T::MaxDelegates>,
 		ValueQuery,
 	>;
@@ -97,22 +93,22 @@ pub mod pallet {
 	pub enum Event<T: Config> {
 		/// A new schema has been created.
 		/// \[schema identifier, version, controller\]
-		Anchor(IdOf<T>, VersionOf, CordAccountOf<T>),
+		Anchor(IdentifierOf, VersionOf, CordAccountOf<T>),
 		/// A schema has been updated.
 		/// \[schema identifier, version, controller\]
-		Update(IdOf<T>, VersionOf, CordAccountOf<T>),
+		Update(IdentifierOf, VersionOf, CordAccountOf<T>),
 		/// A schema status has been changed.
 		/// \[schema identifier, controller\]
-		Status(IdOf<T>, CordAccountOf<T>),
+		Status(IdentifierOf, CordAccountOf<T>),
 		/// Schema delegates has been added.
 		/// \[schema identifier,  controller\]
-		AddDelegates(IdOf<T>, CordAccountOf<T>),
+		AddDelegates(IdentifierOf, CordAccountOf<T>),
 		/// Schema delegates has been removed.
 		/// \[schema identifier,  controller\]
-		RemoveDelegates(IdOf<T>, CordAccountOf<T>),
+		RemoveDelegates(IdentifierOf, CordAccountOf<T>),
 		/// A schema status has been changed.
 		/// \[schema identifier, version, controller\]
-		Permission(IdOf<T>, CordAccountOf<T>),
+		Permission(IdentifierOf, CordAccountOf<T>),
 	}
 
 	#[pallet::error]
@@ -143,6 +139,12 @@ pub mod pallet {
 		SchemaGenesisNotFound,
 		// Only when the author is not the controller
 		UnauthorizedDelegation,
+		// Invalid Identifier
+		InvalidIdentifier,
+		// Invalid Identifier Length
+		InvalidIdentifierLength,
+		// Invalid Identifier Prefix
+		InvalidIdentifierPrefix,
 	}
 
 	#[pallet::call]
@@ -159,7 +161,7 @@ pub mod pallet {
 		#[pallet::weight(126_475_000 + T::DbWeight::get().reads_writes(2, 1))]
 		pub fn authorise(
 			origin: OriginFor<T>,
-			schema: IdOf<T>,
+			schema: IdentifierOf,
 			creator: CordAccountOf<T>,
 			delegates: Vec<CordAccountOf<T>>,
 		) -> DispatchResult {
@@ -198,7 +200,7 @@ pub mod pallet {
 		#[pallet::weight(126_475_000 + T::DbWeight::get().reads_writes(2, 1))]
 		pub fn deauthorise(
 			origin: OriginFor<T>,
-			schema: IdOf<T>,
+			schema: IdentifierOf,
 			creator: CordAccountOf<T>,
 			delegates: Vec<CordAccountOf<T>>,
 		) -> DispatchResult {
@@ -227,10 +229,10 @@ pub mod pallet {
 		/// * schema_hash: hash of the incoming schema stream.
 		/// * cid: \[OPTIONAL\] storage Id of the incoming stream.
 		/// * permissioned: schema type - permissioned or not.
-		#[pallet::weight(570_952_000 + T::DbWeight::get().reads_writes(1, 2))]
+		#[pallet::weight(570_952_000 + T::DbWeight::get().reads_writes(2, 2))]
 		pub fn create(
 			origin: OriginFor<T>,
-			identifier: IdOf<T>,
+			identifier: IdentifierOf,
 			creator: CordAccountOf<T>,
 			version: VersionOf,
 			schema_hash: HashOf<T>,
@@ -238,14 +240,16 @@ pub mod pallet {
 			permissioned: StatusOf,
 		) -> DispatchResult {
 			<T as Config>::EnsureOrigin::ensure_origin(origin)?;
-			ensure!(!<SchemaId<T>>::contains_key(&identifier), Error::<T>::SchemaAlreadyAnchored);
-			Version::parse(str::from_utf8(&version).unwrap())
-				.map_err(|_err| Error::<T>::InvalidSchemaVersion)?;
-
+			ensure!(!<Schemas<T>>::contains_key(&schema_hash), Error::<T>::SchemaAlreadyAnchored);
+			SchemaDetails::<T>::is_valid_identifier(&identifier, SCHEMA_IDENTIFIER_PREFIX)?;
+			Version::parse(
+				str::from_utf8(&version).map_err(|_err| Error::<T>::InvalidSchemaVersion)?,
+			)
+			.map_err(|_err| Error::<T>::InvalidSchemaVersion)?;
 			if let Some(ref cid) = cid {
-				SchemaDetails::<T>::is_valid(cid)?;
+				SchemaDetails::<T>::is_valid_cid(cid)?;
 			}
-
+			ensure!(!<SchemaId<T>>::contains_key(&identifier), Error::<T>::SchemaAlreadyAnchored);
 			<SchemaId<T>>::insert(&identifier, &schema_hash);
 
 			<Schemas<T>>::insert(
@@ -278,7 +282,7 @@ pub mod pallet {
 		#[pallet::weight(191_780_000 + T::DbWeight::get().reads_writes(1, 2))]
 		pub fn version(
 			origin: OriginFor<T>,
-			identifier: IdOf<T>,
+			identifier: IdentifierOf,
 			updater: CordAccountOf<T>,
 			version: VersionOf,
 			schema_hash: HashOf<T>,
@@ -289,11 +293,13 @@ pub mod pallet {
 				<SchemaId<T>>::get(&identifier).ok_or(Error::<T>::SchemaNotFound)?;
 
 			if let Some(ref cid) = cid {
-				SchemaDetails::<T>::is_valid(cid)?;
+				SchemaDetails::<T>::is_valid_cid(cid)?;
 			}
 
-			let new_version = Version::parse(str::from_utf8(&version).unwrap())
-				.map_err(|_err| Error::<T>::InvalidSchemaVersion)?;
+			let new_version = Version::parse(
+				str::from_utf8(&version).map_err(|_err| Error::<T>::InvalidSchemaVersion)?,
+			)
+			.map_err(|_err| Error::<T>::InvalidSchemaVersion)?;
 
 			let schema_details =
 				<Schemas<T>>::get(&prev_schema_hash).ok_or(Error::<T>::SchemaGenesisNotFound)?;
@@ -311,7 +317,7 @@ pub mod pallet {
 				&schema_hash,
 				SchemaDetails {
 					version: version.clone(),
-					schema_id: identifier,
+					schema_id: identifier.clone(),
 					creator: updater.clone(),
 					parent: Some(prev_schema_hash),
 					cid,
@@ -333,7 +339,7 @@ pub mod pallet {
 		#[pallet::weight(124_410_000 + T::DbWeight::get().reads_writes(1, 2))]
 		pub fn status(
 			origin: OriginFor<T>,
-			identifier: IdOf<T>,
+			identifier: IdentifierOf,
 			updater: CordAccountOf<T>,
 			status: StatusOf,
 		) -> DispatchResult {
@@ -361,7 +367,7 @@ pub mod pallet {
 		#[pallet::weight(124_410_000 + T::DbWeight::get().reads_writes(1, 2))]
 		pub fn permission(
 			origin: OriginFor<T>,
-			identifier: IdOf<T>,
+			identifier: IdentifierOf,
 			updater: CordAccountOf<T>,
 			permissioned: StatusOf,
 		) -> DispatchResult {
--- pallets/schema/src/schemas.rs
+++ pallets/schema/src/schemas.rs
@@ -18,6 +18,7 @@
 use crate::*;
 use codec::{Decode, Encode};
 use sp_runtime::DispatchResult;
+use sp_std::str;
 
 /// An on-chain schema details mapped to an identifier.
 #[derive(Clone, Debug, Encode, Decode, PartialEq, scale_info::TypeInfo)]
@@ -26,7 +27,7 @@ pub struct SchemaDetails<T: Config> {
 	/// Schema Version.
 	pub version: VersionOf,
 	/// Schema identifier.
-	pub schema_id: IdOf<T>,
+	pub schema_id: IdentifierOf,
 	/// Schema creator.
 	pub creator: CordAccountOf<T>,
 	/// \[OPTIONAL\] Schema Parent hash.
@@ -40,8 +41,8 @@ pub struct SchemaDetails<T: Config> {
 }
 
 impl<T: Config> SchemaDetails<T> {
-	pub fn is_valid(incoming: &CidOf) -> DispatchResult {
-		let cid_str = str::from_utf8(incoming).unwrap();
+	pub fn is_valid_cid(incoming: &CidOf) -> DispatchResult {
+		let cid_str = str::from_utf8(incoming).map_err(|_err| Error::<T>::InvalidCidEncoding)?;
 		let cid_details: Cid = cid_str.parse().map_err(|_err| Error::<T>::InvalidCidEncoding)?;
 		ensure!(
 			(cid_details.version() == CidType::V1 || cid_details.version() == CidType::V0),
@@ -49,8 +50,29 @@ impl<T: Config> SchemaDetails<T> {
 		);
 		Ok(())
 	}
-
-	pub fn schema_status(tx_schema: &IdOf<T>, requestor: CordAccountOf<T>) -> Result<(), Error<T>> {
+	pub fn is_valid_identifier(id: &IdentifierOf, id_ident: u16) -> DispatchResult {
+		let identifier = str::from_utf8(id).map_err(|_| Error::<T>::InvalidIdentifier)?;
+		let data = identifier.from_base58().map_err(|_| Error::<T>::InvalidIdentifier)?;
+		ensure!(
+			identifier.len() == 48 || identifier.len() == 49,
+			Error::<T>::InvalidIdentifierLength
+		);
+		let (_prefix_len, ident) = match data[0] {
+			0..=63 => (1, data[0] as u16),
+			64..=127 => {
+				let lower = (data[0] << 2) | (data[1] >> 6);
+				let upper = data[1] & 0b00111111;
+				(2, (lower as u16) | ((upper as u16) << 8))
+			},
+			_ => unreachable!("identifier not within the range; qed"),
+		};
+		ensure!(ident == id_ident, Error::<T>::InvalidIdentifier);
+		Ok(())
+	}
+	pub fn schema_status(
+		tx_schema: &IdentifierOf,
+		requestor: CordAccountOf<T>,
+	) -> Result<(), Error<T>> {
 		let schema_hash = <SchemaId<T>>::get(&tx_schema).ok_or(Error::<T>::SchemaNotFound)?;
 		let schema_details = <Schemas<T>>::get(schema_hash).ok_or(Error::<T>::SchemaNotFound)?;
 		ensure!(!schema_details.revoked, Error::<T>::SchemaRevoked);
@@ -63,5 +85,112 @@ impl<T: Config> SchemaDetails<T> {
 			);
 		}
 		Ok(())
+	}
+}
+
+/// Base58-to-text encoding
+///
+/// Based on https://github.com/trezor/trezor-crypto/blob/master/base58.c
+///          https://github.com/debris/base58/blob/master/src/lib.rs
+
+const B58_DIGITS_MAP: &'static [i8] = &[
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, -1, -1, -1, -1, -1, -1, -1, 9, 10, 11, 12, 13, 14, 15, 16, -1,
+	17, 18, 19, 20, 21, -1, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, -1, -1, -1, -1, -1, -1, 33,
+	34, 35, 36, 37, 38, 39, 40, 41, 42, 43, -1, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
+	57, -1, -1, -1, -1, -1,
+];
+// const PREFIX: &[u8] = b"IDFRPRE";
+
+/// Errors that can occur when decoding base58 encoded string.
+#[derive(Debug, PartialEq)]
+pub enum FromBase58Error {
+	/// The input contained a character which is not a part of the base58 format.
+	InvalidBase58Character(char, usize),
+	/// The input had invalid length.
+	InvalidBase58Length,
+}
+
+/// A trait for converting base58 encoded values.
+pub trait FromBase58 {
+	/// Convert a value of `self`, interpreted as base58 encoded data, into an owned vector of bytes, returning a vector.
+	fn from_base58(&self) -> Result<Vec<u8>, FromBase58Error>;
+}
+
+impl FromBase58 for str {
+	fn from_base58(&self) -> Result<Vec<u8>, FromBase58Error> {
+		let mut bin = [0u8; 132];
+		let mut out = [0u32; (132 + 3) / 4];
+		let bytesleft = (bin.len() % 4) as u8;
+		let zeromask = match bytesleft {
+			0 => 0u32,
+			_ => 0xffffffff << (bytesleft * 8),
+		};
+
+		let zcount = self.chars().take_while(|x| *x == '1').count();
+		let mut i = zcount;
+		let b58: Vec<u8> = self.bytes().collect();
+
+		while i < self.len() {
+			if (b58[i] & 0x80) != 0 {
+				// High-bit set on invalid digit
+				return Err(FromBase58Error::InvalidBase58Character(b58[i] as char, i));
+			}
+
+			if B58_DIGITS_MAP[b58[i] as usize] == -1 {
+				// // Invalid base58 digit
+				return Err(FromBase58Error::InvalidBase58Character(b58[i] as char, i));
+			}
+
+			let mut c = B58_DIGITS_MAP[b58[i] as usize] as u64;
+			let mut j = out.len();
+			while j != 0 {
+				j -= 1;
+				let t = out[j] as u64 * 58 + c;
+				c = (t & 0x3f00000000) >> 32;
+				out[j] = (t & 0xffffffff) as u32;
+			}
+
+			if c != 0 {
+				// Output number too big (carry to the next int32)
+				return Err(FromBase58Error::InvalidBase58Length);
+			}
+
+			if (out[0] & zeromask) != 0 {
+				// Output number too big (last int32 filled too far)
+				return Err(FromBase58Error::InvalidBase58Length);
+			}
+
+			i += 1;
+		}
+
+		let mut i = 1;
+		let mut j = 0;
+
+		bin[0] = match bytesleft {
+			3 => ((out[0] & 0xff0000) >> 16) as u8,
+			2 => ((out[0] & 0xff00) >> 8) as u8,
+			1 => {
+				j = 1;
+				(out[0] & 0xff) as u8
+			},
+			_ => {
+				i = 0;
+				bin[0]
+			},
+		};
+
+		while j < out.len() {
+			bin[i] = ((out[j] >> 0x18) & 0xff) as u8;
+			bin[i + 1] = ((out[j] >> 0x10) & 0xff) as u8;
+			bin[i + 2] = ((out[j] >> 8) & 0xff) as u8;
+			bin[i + 3] = ((out[j] >> 0) & 0xff) as u8;
+			i += 4;
+			j += 1;
+		}
+
+		let leading_zeros = bin.iter().take_while(|x| **x == 0).count();
+		Ok(bin[leading_zeros - zcount..].to_vec())
 	}
 }
