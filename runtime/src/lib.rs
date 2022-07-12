d6637920b9d0a9fe2bde4de1e3d3b4fc687cde8c
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2022-07-12 14:34:46 +0530

    Version bump to v0.7.4/7040

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -109,7 +109,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 9010,
+	spec_version: 7040,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
