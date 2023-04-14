3141e60cffadc9895157f0c1dba1c5034869a565
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2023-04-14 12:52:08 +0530

    benchmark updates (#127)
    * schema benchmark updates
    
    * did-name benchmark updates

--- pallets/schema/src/benchmarking.rs
+++ pallets/schema/src/benchmarking.rs
@@ -1,57 +1,68 @@
-use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite};
-use frame_support::{
-	sp_runtime::traits::Hash,
-	traits::{Currency, Get},
-};
+// This file is part of CORD – https://cord.network
+
+// Copyright (C) Dhiway Networks Pvt. Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// CORD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// CORD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with CORD. If not, see <https://www.gnu.org/licenses/>.
+
+#![cfg(feature = "runtime-benchmarks")]
+
+use super::*;
+use codec::Encode;
+use frame_benchmarking::v1::{account, benchmarks, impl_benchmark_test_suite};
+use frame_support::{sp_runtime::traits::Hash, BoundedVec};
 use sp_std::{
 	convert::{TryFrom, TryInto},
-	fmt::Debug,
 	vec::Vec,
 };
 
 use cord_utilities::traits::GenerateBenchmarkOrigin;
 
-use crate::*;
-
 const SEED: u32 = 0;
-const MAX_SCHEMA_SIZE: u32 = 5 * 1024 * 1024;
+const MAX_SCHEMA_SIZE: u32 = 10 * 1024;
 
 benchmarks! {
 	where_clause {
 		where
-		<<T as Config>::Currency as Currency<AccountIdOf<T>>>::Balance: TryFrom<usize>,
-		<<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance as TryFrom<usize>>::Error: Debug,
-		T::EnsureOrigin: GenerateBenchmarkOrigin<T::Origin, T::AccountId, T::SchemaCreatorId>,
+		T::EnsureOrigin: GenerateBenchmarkOrigin<T::RuntimeOrigin, T::AccountId, T::SchemaCreatorId>,
 	}
-
 	create {
 		let l in 1 .. MAX_SCHEMA_SIZE;
 
 		let caller = account("caller", 0, SEED);
 		let did: T::SchemaCreatorId = account("did", 0, SEED);
 
-		let schema: Vec<u8> = (0u8..u8::MAX).cycle().take(l.try_into().unwrap()).collect();
-		let schema_hash = <T as frame_system::Config>::Hashing::hash(&schema[..]);
-		let schema_id: IdentifierOf = generate_schema_id::<Test>(&schema_hash);
+		let raw_schema: Vec<u8> = (0u8..u8::MAX).cycle().take(l.try_into().unwrap()).collect();
+		let schema = BoundedVec::try_from(raw_schema)
+		.expect("Test Schema should fit into the expected input length of for the test runtime.");
+		let digest = <T as frame_system::Config>::Hashing::hash(&schema[..]);
+		let id_digest = <T as frame_system::Config>::Hashing::hash(
+			&[&schema.encode()[..], &did.encode()[..]].concat()[..],
+		);
+		let schema_id = Ss58Identifier::to_schema_id(&(id_digest).encode()[..]).unwrap();
 
-		let initial_balance = <T as Config>::Fee::get() * schema.len().try_into().unwrap() + <T as Config>::Currency::minimum_balance();
-		<T as Config>::Currency::make_free_balance_be(&caller, initial_balance);
 		let origin = T::EnsureOrigin::generate_origin(caller, did.clone());
-
-	}: _<T::Origin>(origin, schema)
+	}: _<T::RuntimeOrigin>(origin, schema)
 	verify {
-		let stored_schema_creator: T::SchemaCreatorId = Schemas::<T>::get(&schema_id).expect("Schema Identifier should be present on chain.");
-		let stored_schema_identifier: T::IdentifierOf = Schema::schema_hashes(&schema_hash)
-				.expect("Schema Hash should be present on chain.");
+		let stored_schema = Schemas::<T>::get(&schema_id).expect("Schema Identifier should be present on chain.");
 		// Verify the Schema has the right owner
-		assert_eq!(stored_schema_creator, did);
-		// Verify the Schema hash is mapped to an identifier
-		assert_eq!(stored_schema_identifier, schema_id);
+		assert_eq!(stored_schema.creator, did);
 	}
 }
 
 impl_benchmark_test_suite! {
 	Pallet,
-	crate::mock::runtime::ExtBuilder::default().build_with_keystore(),
-	crate::mock::runtime::Test
+	crate::mock::new_test_ext(),
+	crate::mock::Test
 }
