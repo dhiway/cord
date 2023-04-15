5f157bcec6c62d9b16997d0233ac6386ece5c22d
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2023-04-15 19:25:35 +0530

    runtime benchmark and test updates 2

--- node/service/tests/benchmark_pallet_works.rs
+++ node/service/tests/benchmark_pallet_works.rs
@@ -15,6 +15,8 @@
 
 // You should have received a copy of the GNU General Public License
 // along with CORD. If not, see <https://www.gnu.org/licenses/>.
+#![cfg(feature = "runtime-benchmarks")]
+
 use assert_cmd::cargo::cargo_bin;
 use std::process::Command;
 
