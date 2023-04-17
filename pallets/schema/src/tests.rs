da61c8ba84f89837195c47ced85904249553ca4e
vjgaur <vijayendra.gaur@gmail.com>
2023-04-17 13:33:47 +0530

    added super

--- pallets/schema/src/tests.rs
+++ pallets/schema/src/tests.rs
@@ -15,6 +15,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with CORD. If not, see <https://www.gnu.org/licenses/>.
+use super::*;
 #[cfg(any(feature = "mock", test))]
 use crate::mock as crate_mock;
 use crate::*;
