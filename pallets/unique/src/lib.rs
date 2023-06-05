bca22905cb1aab8f72b6b5ac5d21c9352efd7a67
Rohit Bhandari <jainrohit090@gmail.com>
2023-06-05 15:56:10 +0530

    Added Pallet Unique Structure

---
+++ pallets/unique/src/lib.rs
@@ -0,0 +1,79 @@
+// This file is part of CORD – https://cord.network
+
+// Copyright (C) Dhiway Networks Pvt. Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// CORD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// CORD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with CORD. If not, see <https://www.gnu.org/licenses/>.
+
+//! # Unique Pallet
+//!
+//!The digest anchoring functionality is implemented within the
+//! stream pallet, which does not support revoke/restore.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![allow(clippy::unused_unit)]
+
+#[cfg(feature = "runtime-benchmarks")]
+pub mod benchmarking;
+
+// #[cfg(any(feature = "mock", test))]
+// pub mod mock;
+
+// #[cfg(test)]
+// pub mod tests;
+
+use cord_primitives::{curi::Ss58Identifier, StatusOf};
+use frame_support::{ensure, storage::types::StorageMap};
+use sp_std::{prelude::Clone, str};
+pub mod types;
+pub mod weights;
+pub use crate::types::*;
+
+pub use crate::{pallet::*, types::*, weights::WeightInfo};
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+	use cord_utilities::traits::CallSources;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+	use sp_runtime::traits::Hash;
+
+	/// The current storage version.
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+
+	#[pallet::pallet]
+	#[pallet::storage_version(STORAGE_VERSION)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {}
+
+	#[pallet::error]
+	pub enum Error<T> {}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {}
+}
+
+impl<T: Config> Pallet<T> {}
