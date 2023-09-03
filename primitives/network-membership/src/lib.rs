560b3ef086f58ee30ac712025975de8aa2c6d14a
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2023-09-03 18:35:00 +0530

    authorsip -> network-membership refactor

---
+++ primitives/network-membership/src/lib.rs
@@ -0,0 +1,51 @@
+// This file is part of CORD – https://cord.network
+
+// Copyright (C) Dhiway Networks Pvt. Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// CORD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// CORD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with CORD. If not, see <https://www.gnu.org/licenses/>.
+
+//! Defines types and traits fo pallet network membership.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![allow(clippy::type_complexity)]
+
+pub mod traits;
+
+use codec::{Decode, Encode, MaxEncodedLen};
+use frame_support::RuntimeDebug;
+use scale_info::TypeInfo;
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+
+pub enum Event<AccountId = ()> {
+	/// A membership has acquired
+	MembershipAcquired(AccountId),
+	/// A membership has expired
+	MembershipExpired(AccountId),
+	/// A membership has renewed
+	MembershipRenewed(AccountId),
+	/// A membership has revoked
+	MembershipRevoked(AccountId),
+	/// A membership renew request
+	MembershipRenewRequest(AccountId),
+}
+
+#[cfg_attr(feature = "std", derive(Deserialize, Serialize))]
+#[derive(
+	Encode, Decode, Default, Clone, Copy, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen,
+)]
+pub struct MemberData<BlockNumber: Decode + Encode + TypeInfo> {
+	pub expires_on: BlockNumber,
+}
