da143596c00890fc517cdd0f35bf6dac3774f1c8
roynrishingha <roynrishingha@gmail.com>
2023-09-21 16:02:40 +0530

    Bump up CORD Runtime version to 0.9.0

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -117,7 +117,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("cord"),
 	impl_name: create_runtime_str!("dhiway-cord"),
 	authoring_version: 0,
-	spec_version: 8200,
+	spec_version: 9000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
