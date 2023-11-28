429b8da21e72470831e667a70bc47d845ba08d2e
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2023-11-28 10:00:30 +0530

    score refactor (#252)

--- pallets/score/src/mock.rs
+++ pallets/score/src/mock.rs
@@ -1,136 +1,136 @@
-// This file is part of CORD – https://cord.network
+// // This file is part of CORD – https://cord.network
 
-// Copyright (C) Dhiway Networks Pvt. Ltd.
-// SPDX-License-Identifier: GPL-3.0-or-later
+// // Copyright (C) Dhiway Networks Pvt. Ltd.
+// // SPDX-License-Identifier: GPL-3.0-or-later
 
-// CORD is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// // CORD is free software: you can redistribute it and/or modify
+// // it under the terms of the GNU General Public License as published by
+// // the Free Software Foundation, either version 3 of the License, or
+// // (at your option) any later version.
 
-// CORD is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+// // CORD is distributed in the hope that it will be useful,
+// // but WITHOUT ANY WARRANTY; without even the implied warranty of
+// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// // GNU General Public License for more details.
 
-// You should have received a copy of the GNU General Public License
-// along with CORD. If not, see <https://www.gnu.org/licenses/>.
+// // You should have received a copy of the GNU General Public License
+// // along with CORD. If not, see <https://www.gnu.org/licenses/>.
 
-use super::*;
-use crate as pallet_score;
-use cord_utilities::mock::{mock_origin, SubjectId};
-use frame_support::{
-	construct_runtime, parameter_types,
-	traits::{ConstU32, ConstU64},
-};
-use sp_runtime::{
-	traits::{BlakeTwo256, IdentifyAccount, IdentityLookup, Verify},
-	BuildStorage, MultiSignature,
-};
+// use super::*;
+// use crate as pallet_score;
+// use cord_utilities::mock::{mock_origin, SubjectId};
+// use frame_support::{
+// 	construct_runtime, parameter_types,
+// 	traits::{ConstU32, ConstU64},
+// };
+// use sp_runtime::{
+// 	traits::{BlakeTwo256, IdentifyAccount, IdentityLookup, Verify},
+// 	BuildStorage, MultiSignature,
+// };
 
-type Hash = sp_core::H256;
-type Signature = MultiSignature;
-type AccountPublic = <Signature as Verify>::Signer;
-pub type AccountId = <AccountPublic as IdentifyAccount>::AccountId;
-pub(crate) type Block = frame_system::mocking::MockBlock<Test>;
+// type Hash = sp_core::H256;
+// type Signature = MultiSignature;
+// type AccountPublic = <Signature as Verify>::Signer;
+// pub type AccountId = <AccountPublic as IdentifyAccount>::AccountId;
+// pub(crate) type Block = frame_system::mocking::MockBlock<Test>;
 
-construct_runtime!(
-	pub enum Test
-	{
-		System: frame_system,
-		Schema: pallet_schema,
-		Registry: pallet_chain_space,
-		Score: pallet_score::{Pallet, Call, Storage, Event<T>},
-		MockOrigin: mock_origin,
-	}
-);
+// construct_runtime!(
+// 	pub enum Test
+// 	{
+// 		System: frame_system,
+// 		Schema: pallet_schema,
+// 		Registry: pallet_chain_space,
+// 		Score: pallet_score::{Pallet, Call, Storage, Event<T>},
+// 		MockOrigin: mock_origin,
+// 	}
+// );
 
-parameter_types! {
-	pub const SS58Prefix: u8 = 29;
-}
-impl frame_system::Config for Test {
-	type RuntimeOrigin = RuntimeOrigin;
-	type RuntimeCall = RuntimeCall;
-	type Block = Block;
-	type Nonce = u32;
-	type Hash = Hash;
-	type Hashing = BlakeTwo256;
-	type AccountId = AccountId;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type RuntimeEvent = RuntimeEvent;
-	type BlockHashCount = ConstU64<250>;
-	type DbWeight = ();
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = ();
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type BaseCallFilter = frame_support::traits::Everything;
-	type SystemWeightInfo = ();
-	type BlockWeights = ();
-	type BlockLength = ();
-	type SS58Prefix = SS58Prefix;
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
-}
+// parameter_types! {
+// 	pub const SS58Prefix: u8 = 29;
+// }
+// impl frame_system::Config for Test {
+// 	type RuntimeOrigin = RuntimeOrigin;
+// 	type RuntimeCall = RuntimeCall;
+// 	type Block = Block;
+// 	type Nonce = u32;
+// 	type Hash = Hash;
+// 	type Hashing = BlakeTwo256;
+// 	type AccountId = AccountId;
+// 	type Lookup = IdentityLookup<Self::AccountId>;
+// 	type RuntimeEvent = RuntimeEvent;
+// 	type BlockHashCount = ConstU64<250>;
+// 	type DbWeight = ();
+// 	type Version = ();
+// 	type PalletInfo = PalletInfo;
+// 	type AccountData = ();
+// 	type OnNewAccount = ();
+// 	type OnKilledAccount = ();
+// 	type BaseCallFilter = frame_support::traits::Everything;
+// 	type SystemWeightInfo = ();
+// 	type BlockWeights = ();
+// 	type BlockLength = ();
+// 	type SS58Prefix = SS58Prefix;
+// 	type OnSetCode = ();
+// 	type MaxConsumers = frame_support::traits::ConstU32<16>;
+// }
 
-impl mock_origin::Config for Test {
-	type RuntimeOrigin = RuntimeOrigin;
-	type AccountId = AccountId;
-	type SubjectId = SubjectId;
-}
+// impl mock_origin::Config for Test {
+// 	type RuntimeOrigin = RuntimeOrigin;
+// 	type AccountId = AccountId;
+// 	type SubjectId = SubjectId;
+// }
 
-impl Config for Test {
-	type RatingCreatorIdOf = SubjectId;
-	type EnsureOrigin = mock_origin::EnsureDoubleOrigin<AccountId, SubjectId>;
-	type OriginSuccess = mock_origin::DoubleOrigin<AccountId, SubjectId>;
-	type RuntimeEvent = RuntimeEvent;
-	type WeightInfo = weights::SubstrateWeight<Test>;
-	type ValueLimit = ConstU32<72>;
-}
+// impl Config for Test {
+// 	type RatingCreatorIdOf = SubjectId;
+// 	type EnsureOrigin = mock_origin::EnsureDoubleOrigin<AccountId, SubjectId>;
+// 	type OriginSuccess = mock_origin::DoubleOrigin<AccountId, SubjectId>;
+// 	type RuntimeEvent = RuntimeEvent;
+// 	type WeightInfo = weights::SubstrateWeight<Test>;
+// 	type ValueLimit = ConstU32<72>;
+// }
 
-parameter_types! {
-	#[derive(Debug, Clone)]
-	pub const MaxEncodedRegistryLength: u32 = 15_360;
-	pub const MaxRegistryAuthorities: u32 = 3u32;
-	#[derive(Debug, Clone)]
-	pub const MaxRegistryCommitActions: u32 = 5u32;
-}
+// parameter_types! {
+// 	#[derive(Debug, Clone)]
+// 	pub const MaxEncodedRegistryLength: u32 = 15_360;
+// 	pub const MaxRegistryAuthorities: u32 = 3u32;
+// 	#[derive(Debug, Clone)]
+// 	pub const MaxRegistryCommitActions: u32 = 5u32;
+// }
 
-impl pallet_chain_space::Config for Test {
-	type RuntimeEvent = RuntimeEvent;
-	type EnsureOrigin = mock_origin::EnsureDoubleOrigin<AccountId, SubjectId>;
-	type OriginSuccess = mock_origin::DoubleOrigin<AccountId, SubjectId>;
-	type RegistryCreatorId = SubjectId;
-	type MaxEncodedRegistryLength = MaxEncodedRegistryLength;
-	type MaxRegistryAuthorities = MaxRegistryAuthorities;
-	type MaxRegistryCommitActions = MaxRegistryCommitActions;
-	type WeightInfo = pallet_chain_space::weights::SubstrateWeight<Test>;
-}
+// impl pallet_chain_space::Config for Test {
+// 	type RuntimeEvent = RuntimeEvent;
+// 	type EnsureOrigin = mock_origin::EnsureDoubleOrigin<AccountId, SubjectId>;
+// 	type OriginSuccess = mock_origin::DoubleOrigin<AccountId, SubjectId>;
+// 	type RegistryCreatorId = SubjectId;
+// 	type MaxEncodedRegistryLength = MaxEncodedRegistryLength;
+// 	type MaxRegistryAuthorities = MaxRegistryAuthorities;
+// 	type MaxRegistryCommitActions = MaxRegistryCommitActions;
+// 	type WeightInfo = pallet_chain_space::weights::SubstrateWeight<Test>;
+// }
 
-parameter_types! {
-	pub const MaxEncodedSchemaLength: u32 = 15_360;
-}
+// parameter_types! {
+// 	pub const MaxEncodedSchemaLength: u32 = 15_360;
+// }
 
-impl pallet_schema::Config for Test {
-	type SchemaCreatorId = SubjectId;
-	type EnsureOrigin = mock_origin::EnsureDoubleOrigin<AccountId, SubjectId>;
-	type OriginSuccess = mock_origin::DoubleOrigin<AccountId, SubjectId>;
-	type RuntimeEvent = RuntimeEvent;
-	type WeightInfo = ();
-	type MaxEncodedSchemaLength = MaxEncodedSchemaLength;
-}
+// impl pallet_schema::Config for Test {
+// 	type SchemaCreatorId = SubjectId;
+// 	type EnsureOrigin = mock_origin::EnsureDoubleOrigin<AccountId, SubjectId>;
+// 	type OriginSuccess = mock_origin::DoubleOrigin<AccountId, SubjectId>;
+// 	type RuntimeEvent = RuntimeEvent;
+// 	type WeightInfo = ();
+// 	type MaxEncodedSchemaLength = MaxEncodedSchemaLength;
+// }
 
-#[allow(dead_code)]
-pub(crate) fn new_test_ext() -> sp_io::TestExternalities {
-	let t: sp_runtime::Storage =
-		frame_system::GenesisConfig::<Test>::default().build_storage().unwrap();
+// #[allow(dead_code)]
+// pub(crate) fn new_test_ext() -> sp_io::TestExternalities {
+// 	let t: sp_runtime::Storage =
+// 		frame_system::GenesisConfig::<Test>::default().build_storage().unwrap();
 
-	let mut ext = sp_io::TestExternalities::new(t);
-	#[cfg(feature = "runtime-benchmarks")]
-	let keystore = sp_keystore::testing::MemoryKeystore::new();
-	#[cfg(feature = "runtime-benchmarks")]
-	ext.register_extension(sp_keystore::KeystoreExt(sp_std::sync::Arc::new(keystore)));
-	ext.execute_with(|| System::set_block_number(1));
-	ext
-}
+// 	let mut ext = sp_io::TestExternalities::new(t);
+// 	#[cfg(feature = "runtime-benchmarks")]
+// 	let keystore = sp_keystore::testing::MemoryKeystore::new();
+// 	#[cfg(feature = "runtime-benchmarks")]
+// 	ext.register_extension(sp_keystore::KeystoreExt(sp_std::sync::Arc::new(keystore)));
+// 	ext.execute_with(|| System::set_block_number(1));
+// 	ext
+// }
