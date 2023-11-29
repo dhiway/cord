50032a5f34716fdeacaca6db6255d8e1c3b89546
Amar Tumballi <amar@dhiway.com>
2023-11-29 22:17:04 +0530

    add pallet-score: test and clippy passing without benchmark
    Signed-off-by: Amar Tumballi <amar@dhiway.com>

--- pallets/score/src/benchmarking.rs
+++ pallets/score/src/benchmarking.rs
@@ -1,147 +1,134 @@
-// // This file is part of CORD – https://cord.network
+// This file is part of CORD – https://cord.network
 
-// // Copyright (C) Dhiway Networks Pvt. Ltd.
-// // SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) Dhiway Networks Pvt. Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later
 
-// // CORD is free software: you can redistribute it and/or modify
-// // it under the terms of the GNU General Public License as published by
-// // the Free Software Foundation, either version 3 of the License, or
-// // (at your option) any later version.
+// CORD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 
-// // CORD is distributed in the hope that it will be useful,
-// // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// // GNU General Public License for more details.
+// CORD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 
-// // You should have received a copy of the GNU General Public License
-// // along with CORD. If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with CORD. If not, see <https://www.gnu.org/licenses/>.
 
-// #![cfg(feature = "runtime-benchmarks")]
+#![cfg(feature = "runtime-benchmarks")]
 
-// use super::*;
-// use codec::Encode;
-// use cord_primitives::curi::Ss58Identifier;
-// use cord_utilities::traits::GenerateBenchmarkOrigin;
-// use frame_benchmarking::{account, benchmarks};
-// use frame_support::{sp_runtime::traits::Hash, BoundedVec};
-// use pallet_chain_space::{
-// 	Authorizations, InputRegistryOf, Permissions, RegistryAuthorizationOf,
-// RegistryHashOf, };
-// use sp_std::{convert::TryFrom, vec::Vec};
+use super::*;
+use codec::Encode;
+use cord_primitives::curi::Ss58Identifier;
+use cord_utilities::traits::GenerateBenchmarkOrigin;
+use frame_benchmarking::{account, benchmarks};
+use frame_support::BoundedVec;
+use pallet_chain_space::SpaceCodeOf;
 
-// const SEED: u32 = 0;
-// const MAX_PAYLOAD_BYTE_LENGTH: u32 = 15 * 1024;
+const SEED: u32 = 0;
+const MAX_PAYLOAD_BYTE_LENGTH: u32 = 15 * 1024;
 
-// pub fn generate_registry_id<T: Config>(other_digest: &RegistryHashOf<T>) ->
-// RegistryIdOf { 	Ss58Identifier::to_registry_id(&(other_digest).encode()[..]).
-// unwrap() }
+pub fn generate_space_id<T: Config>(other_digest: &SpaceCodeOf<T>) -> SpaceIdOf {
+	Ss58Identifier::to_space_id(&(other_digest).encode()[..]).unwrap()
+}
 
-// pub fn generate_rating_id<T: Config>(other_digest: &RatingEntryHashOf<T>) ->
-// RatingIdOf { 	Ss58Identifier::to_scoring_id(&(other_digest).encode()[..]).
-// unwrap() }
-// fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
-// 	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
-// }
+pub fn generate_rating_id<T: Config>(other_digest: &RatingEntryHashOf<T>) -> RatingEntryIdOf {
+	Ss58Identifier::to_scoring_id(&(other_digest).encode()[..]).unwrap()
+}
 
-// benchmarks! {
-// 	where_clause {
-// 		where
-// 		<T as pallet::Config>::EnsureOrigin:
-// GenerateBenchmarkOrigin<T::RuntimeOrigin, T::AccountId,
-// T::RegistryCreatorId>, 	}
-// 	add_rating {
-// 		let l in 1 .. MAX_PAYLOAD_BYTE_LENGTH;
+fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
+	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
+}
 
-// 		let caller: T::AccountId = account("caller", 0, SEED);
-// 		let did: T::RegistryCreatorId = account("did", 0, SEED);
-// 		let did1: T::RegistryCreatorId = account("did1", 0, SEED);
+benchmarks! {
+	where_clause {
+		where
+		<T as pallet::Config>::EnsureOrigin: GenerateBenchmarkOrigin<T::RuntimeOrigin, T::AccountId, T::SpaceCreatorId>,
+	}
+	register_rating {
+		let l in 1 .. MAX_PAYLOAD_BYTE_LENGTH;
 
-// 		let rating: Vec<u8> = [2u8; 256].to_vec();
-// 		let rating_digest = <T as frame_system::Config>::Hashing::hash(&rating[..]);
+		let caller: T::AccountId = account("caller", 0, SEED);
+		let did: T::SpaceCreatorId = account("did", 0, SEED);
+		let did1: T::SpaceCreatorId = account("did1", 0, SEED);
 
-// 		//EntityIdentifierOf
-// 		let e_id: EntityIdentifierOf<T> = caller.clone();
-// 		//TransactionIdentfierOf
-// 		let raw_transaction_id = [12u8; 72].to_vec();
-// 		let t_id: TransactionIdentifierOf<T> =
-// BoundedVec::try_from(raw_transaction_id).unwrap(); 		//CollectorIdentifierOf
-// 		let c_id: CollectorIdentifierOf<T> = caller.clone();
-// 		//ScoreTypeOf
-// 		let rating_type: RatingTypeOf = RatingTypeOf::Overall;
-// 		//Entity Rating
-// 		let rating: RatingOf = 4;
-// 		//count
-// 		let count : CountOf = 4;
-// 		//entry type
-// 		let entry_type: RatingEntryType = RatingEntryType::Credit;
+		let raw_registry = [56u8; 256].to_vec();
+		let registry = BoundedVec::try_from(raw_registry).unwrap();
+		let id_digest = <T as frame_system::Config>::Hashing::hash(
+			&[&registry.encode()[..], &did.encode()[..]].concat()[..],
+		);
+		let registry_id: SpaceIdOf = generate_space_id::<T>(&id_digest);
+		let message_id = BoundedVec::try_from([72u8; 10].to_vec()).unwrap();
+		let entity_uid = BoundedVec::try_from([73u8; 10].to_vec()).unwrap();
+		let provider_uid = BoundedVec::try_from([74u8; 10].to_vec()).unwrap();
+		let entry = RatingInputEntryOf::<T> {
+			entity_uid,
+			provider_uid,
+			total_rating: 250u64,
+			count_of_txn: 7u64,
+			entity_type: EntityTypeOf::Logistic,
+			rating_type: RatingTypeOf::Overall,
+		};
+		let entry_digest = <T as frame_system::Config>::Hashing::hash(
+			&[&entry.encode()[..]].concat()[..],
+		);
 
-// 		let journal_details = RatingEntryDetails {
-// 		entity: e_id,
-// 		tid: t_id,
-// 		collector: c_id,
-// 		rating_type,
-// 		rating,
-// 		entry_type,
-// 		count,
-// 		};
+		let identifier = Ss58Identifier::to_scoring_id(&(entry_digest.clone()).encode()[..]).unwrap();
 
-// 	let journal_details_digest = <T as frame_system::Config>::Hashing::hash(
-// 		&[&journal_details.encode()[..]].concat()[..],
-// 	);
+		let auth_digest = <T as frame_system::Config>::Hashing::hash(
+			&[&registry_id.encode()[..], &did1.encode()[..], &caller.encode()[..]].concat()[..],
+		);
+		let authorization_id: AuthorizationIdOf =
+			Ss58Identifier::to_authorization_id(&auth_digest.encode()[..]).unwrap();
 
-// 	let raw_registry = [56u8; 256].to_vec();
-// 	let registry: InputRegistryOf<T> =
-// BoundedVec::try_from(raw_registry).unwrap(); 	let id_digest = <T as
-// frame_system::Config>::Hashing::hash( 		&[&registry.encode()[..],
-// &did.encode()[..]].concat()[..], 	);
-// 	let registry_id: RegistryIdOf = generate_registry_id::<T>(&id_digest);
+		let origin =  <T as pallet::Config>::EnsureOrigin::generate_origin(caller,did1.clone());
+	 }: _<T::RuntimeOrigin>(origin, entry, entry_digest, message_id, authorization_id)
+	verify {
+		assert_last_event::<T>(Event::RatingEntryAdded { identifier, entity: entity_uid.clone()}.into());
+	}
 
-// 	let journal_entry = RatingEntry {
-// 		entry: journal_details.clone(),
-// 		digest: journal_details_digest,
-// 		created_at: 1,
-// 		registry: registry_id.clone(),
-// 		creator: did.clone(),
-// 	};
+	amend_rating {
+		let l in 1 .. MAX_PAYLOAD_BYTE_LENGTH;
 
-// 	let journal_entry_digest =
-// 		<T as frame_system::Config>::Hashing::hash(&
-// 			[&journal_entry.digest.encode()[..]].concat()[..]);
+		let caller: T::AccountId = account("caller", 0, SEED);
+		let did: T::SpaceCreatorId = account("did", 0, SEED);
+		let did1: T::SpaceCreatorId = account("did1", 0, SEED);
 
-// 	let identifier =
-// Ss58Identifier::to_scoring_id(&(journal_entry_digest.clone()).encode()[..]).
-// unwrap();
+		let raw_registry = [56u8; 256].to_vec();
+		let registry= BoundedVec::try_from(raw_registry).unwrap();
+		let id_digest = <T as frame_system::Config>::Hashing::hash(
+			&[&registry.encode()[..], &did.encode()[..]].concat()[..],
+		);
+		let registry_id: SpaceIdOf = generate_space_id::<T>(&id_digest);
+		let message_id = BoundedVec::try_from([72u8; 10].to_vec()).unwrap();
+		let entity_uid = BoundedVec::try_from([73u8; 10].to_vec()).unwrap();
+		let provider_uid = BoundedVec::try_from([74u8; 10].to_vec()).unwrap();
+		let entry = RatingInputEntryOf::<T> {
+			entity_uid,
+			provider_uid,
+			total_rating: 250u64,
+			count_of_txn: 7u64,
+			entity_type: EntityTypeOf::Logistic,
+			rating_type: RatingTypeOf::Overall,
+		};
+		let entry_digest = <T as frame_system::Config>::Hashing::hash(
+			&[&entry.encode()[..]].concat()[..],
+		);
 
-// 	let journal_input = RatingInput {
-// 		entry: journal_details,
-// 		digest: journal_entry_digest,
-// 		creator: did,
-// 	};
+		let identifier = Ss58Identifier::to_scoring_id(&(entry_digest.clone()).encode()[..]).unwrap();
 
-// 	let auth_digest = <T as frame_system::Config>::Hashing::hash(
-// 		&[&registry_id.encode()[..], &did1.encode()[..],
-// &caller.encode()[..]].concat()[..], 	);
-// 	let authorization_id: AuthorizationIdOf =
-// 		Ss58Identifier::to_authorization_id(&auth_digest.encode()[..]).unwrap();
+		let auth_digest = <T as frame_system::Config>::Hashing::hash(
+			&[&registry_id.encode()[..], &did1.encode()[..], &caller.encode()[..]].concat()[..],
+		);
+		let authorization_id: AuthorizationIdOf =
+			Ss58Identifier::to_authorization_id(&auth_digest.encode()[..]).unwrap();
 
-// 		<Authorizations<T>>::insert(
-// 			&authorization_id,
-// 			RegistryAuthorizationOf::<T> {
-// 				registry_id,
-// 				delegate: did1.clone(),
-// 				schema: None,
-// 				permissions: Permissions::all(),
-// 			},
-// 		);
+		let origin =  <T as pallet::Config>::EnsureOrigin::generate_origin(caller,did1.clone());
+	 }: _<T::RuntimeOrigin>(origin, identifier, message_id, entry_digest, authorization_id)
+	verify {
+		assert_last_event::<T>(Event::RatingEntryRevoked { identifier, entity: entity_uid.clone()}.into());
+	}
 
-// 		let origin =  <T as pallet::Config>::EnsureOrigin::generate_origin(caller,
-// did1.clone()); 	}: _<T::RuntimeOrigin>(origin, journal_input
-// ,authorization_id) 	verify {
-// 		assert_last_event::<T>(Event::JournalEntry { identifier,entity:
-// journal_entry.entry.entity.clone(),author: did1}.into()); 	}
-// 	impl_benchmark_test_suite! (
-// 		Pallet,
-// 		crate::mock::new_test_ext(),
-// 		crate::mock::Test
-// 	)
-// }
+	impl_benchmark_test_suite! (Pallet, crate::mock::new_test_ext(), crate::mock::Test)
+}
--- pallets/score/src/tests.rs
+++ pallets/score/src/tests.rs
@@ -1,130 +1,91 @@
-// // This file is part of CORD – https://cord.network
+// This file is part of CORD – https://cord.network
 
-// // Copyright (C) Dhiway Networks Pvt. Ltd.
-// // SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) Dhiway Networks Pvt. Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later
 
-// // CORD is free software: you can redistribute it and/or modify
-// // it under the terms of the GNU General Public License as published by
-// // the Free Software Foundation, either version 3 of the License, or
-// // (at your option) any later version.
+// CORD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 
-// // CORD is distributed in the hope that it will be useful,
-// // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// // GNU General Public License for more details.
+// CORD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 
-// // You should have received a copy of the GNU General Public License
-// // along with CORD. If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with CORD. If not, see <https://www.gnu.org/licenses/>.
 
-// use super::*;
-// use crate::mock::*;
-// use codec::Encode;
-// use cord_utilities::mock::{mock_origin::DoubleOrigin, SubjectId};
-// use frame_support::{assert_ok, BoundedVec};
-// use pallet_chain_space::{InputRegistryOf, RegistryHashOf};
-// use sp_runtime::{traits::Hash, AccountId32};
-// use sp_std::prelude::*;
+use super::*;
+use crate::mock::*;
+use codec::Encode;
+use cord_utilities::mock::{mock_origin::DoubleOrigin, SubjectId};
+use frame_support::{assert_ok, BoundedVec};
+use frame_system::RawOrigin;
+use pallet_chain_space::SpaceCodeOf;
+use sp_runtime::{traits::Hash, AccountId32};
+use sp_std::prelude::*;
 
-// pub fn generate_rating_id<T: Config>(digest: &RatingEntryHashOf<T>) ->
-// RatingIdOf { 	Ss58Identifier::to_scoring_id(&(digest).encode()[..]).unwrap()
-// }
+pub fn generate_rating_id<T: Config>(digest: &RatingEntryHashOf<T>) -> RatingEntryIdOf {
+	Ss58Identifier::to_scoring_id(&(digest).encode()[..]).unwrap()
+}
 
-// pub fn generate_registry_id<T: Config>(digest: &RegistryHashOf<T>) ->
-// RegistryIdOf { 	Ss58Identifier::to_registry_id(&(digest).encode()[..]).
-// unwrap() }
+pub fn generate_space_id<T: Config>(digest: &SpaceCodeOf<T>) -> SpaceIdOf {
+	Ss58Identifier::to_space_id(&(digest).encode()[..]).unwrap()
+}
 
-// pub(crate) const DID_00: SubjectId = SubjectId(AccountId32::new([1u8; 32]));
-// pub(crate) const DID_01: SubjectId = SubjectId(AccountId32::new([5u8; 32]));
-// pub(crate) const ACCOUNT_00: AccountId = AccountId::new([1u8; 32]);
+pub(crate) const DID_00: SubjectId = SubjectId(AccountId32::new([1u8; 32]));
+pub(crate) const ACCOUNT_00: AccountId = AccountId::new([1u8; 32]);
 
-// #[test]
-// fn check_successful_rating_creation() {
-// 	let creator = DID_00;
-// 	let author = ACCOUNT_00;
-// 	let delegate = DID_01;
+#[test]
+fn check_successful_rating_creation() {
+	let creator = DID_00;
+	let author = ACCOUNT_00;
 
-// 	//EntityIdentifierOf
-// 	let e_id: EntityIdentifierOf<Test> = ACCOUNT_00;
-// 	//TransactionIdentfierOf
-// 	let raw_transaction_id = [12u8; 72].to_vec();
-// 	let t_id: TransactionIdentifierOf<Test> =
-// BoundedVec::try_from(raw_transaction_id).unwrap(); 	//CollectorIdentifierOf
-// 	let c_id: CollectorIdentifierOf<Test> = ACCOUNT_00;
-// 	//ScoreTypeOf
-// 	let rating_type: RatingTypeOf = RatingTypeOf::Overall;
-// 	//Entity Rating
-// 	let rating: RatingOf = 40;
-// 	//Count
-// 	let count: CountOf = 1;
-// 	//Entry type
-// 	let entry_type: RatingEntryType = RatingEntryType::Credit;
+	let message_id = BoundedVec::try_from([72u8; 10].to_vec()).unwrap();
+	let entity_uid = BoundedVec::try_from([73u8; 10].to_vec()).unwrap();
+	let provider_uid = BoundedVec::try_from([74u8; 10].to_vec()).unwrap();
+	let entry = RatingInputEntryOf::<Test> {
+		entity_uid,
+		provider_uid,
+		total_rating: 250u64,
+		count_of_txn: 7u64,
+		entity_type: EntityTypeOf::Logistic,
+		rating_type: RatingTypeOf::Overall,
+	};
+	let entry_digest =
+		<Test as frame_system::Config>::Hashing::hash(&[&entry.encode()[..]].concat()[..]);
 
-// 	let journal_details = RatingEntryDetails {
-// 		entity: e_id,
-// 		tid: t_id,
-// 		collector: c_id,
-// 		rating_type,
-// 		rating,
-// 		count,
-// 		entry_type,
-// 	};
+	let raw_space = [2u8; 256].to_vec();
+	let space_digest = <Test as frame_system::Config>::Hashing::hash(&raw_space.encode()[..]);
+	let space_id_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&space_digest.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+	let space_id: SpaceIdOf = generate_space_id::<Test>(&space_id_digest);
 
-// 	let journal_details_digest = <Test as frame_system::Config>::Hashing::hash(
-// 		&[&journal_details.encode()[..]].concat()[..],
-// 	);
+	let auth_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&space_id.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+	let authorization_id: AuthorizationIdOf =
+		Ss58Identifier::to_authorization_id(&auth_digest.encode()[..]).unwrap();
 
-// 	let raw_registry = [56u8; 256].to_vec();
-// 	let registry: InputRegistryOf<Test> =
-// BoundedVec::try_from(raw_registry).unwrap(); 	let id_digest = <Test as
-// frame_system::Config>::Hashing::hash( 		&[&registry.encode()[..],
-// &creator.encode()[..]].concat()[..], 	);
-// 	let registry_id: RegistryIdOf = generate_registry_id::<Test>(&id_digest);
+	new_test_ext().execute_with(|| {
+		System::set_block_number(1);
+		// Author Transaction
 
-// 	let journal_entry = RatingEntry {
-// 		entry: journal_details.clone(),
-// 		digest: journal_details_digest,
-// 		created_at: 1,
-// 		registry: registry_id.clone(),
-// 		creator: creator.clone(),
-// 	};
+		assert_ok!(Space::create(
+			DoubleOrigin(author.clone(), creator.clone()).into(),
+			space_digest,
+		));
 
-// 	let journal_entry_digest =
-// 		<Test as
-// frame_system::Config>::Hashing::hash(&[&journal_entry.encode()[..]].
-// concat()[..]);
+		assert_ok!(Space::approve(RawOrigin::Root.into(), space_id, 3u64));
 
-// 	let journal_input = RatingInput {
-// 		entry: journal_details,
-// 		digest: journal_entry_digest,
-// 		creator: creator.clone(),
-// 	};
-
-// 	let auth_digest = <Test as frame_system::Config>::Hashing::hash(
-// 		&[&registry_id.encode()[..], &delegate.encode()[..],
-// &author.encode()[..]].concat()[..], 	);
-// 	let authorization_id: AuthorizationIdOf =
-// 		Ss58Identifier::to_authorization_id(&auth_digest.encode()[..]).unwrap();
-
-// 	new_test_ext().execute_with(|| {
-// 		System::set_block_number(1);
-// 		// Author Transaction
-
-// 		assert_ok!(Registry::create(
-// 			DoubleOrigin(author.clone(), creator.clone()).into(),
-// 			registry.clone(),
-// 			None
-// 		));
-
-// 		assert_ok!(Registry::add_admin_delegate(
-// 			DoubleOrigin(author.clone(), creator.clone()).into(),
-// 			registry_id.clone(),
-// 			delegate.clone(),
-// 		));
-
-// 		assert_ok!(Score::add_rating(
-// 			DoubleOrigin(author.clone(), delegate.clone()).into(),
-// 			journal_input.clone(),
-// 			authorization_id
-// 		));
-// 	});
-// }
+		assert_ok!(Score::register_rating(
+			DoubleOrigin(author.clone(), creator.clone()).into(),
+			entry,
+			entry_digest,
+			message_id,
+			authorization_id,
+		));
+	});
+}
