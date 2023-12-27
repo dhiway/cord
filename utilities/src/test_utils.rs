eeec361d1198f2a5c69e9e87aecef218b2512c48
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2023-12-27 19:24:43 +0530

    DID try-state and toolchain defaults -2

---
+++ utilities/src/test_utils.rs
@@ -0,0 +1,28 @@
+// This file is part of CORD – https://cord.network
+
+// Copyright (C) 2019-2023 BOTLabs GmbH.
+// Copyright (C) Dhiway Networks Pvt. Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Adapted to meet the requirements of the CORD project.
+
+// CORD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// CORD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with CORD. If not, see <https://www.gnu.org/licenses/>.
+
+use scale_info::prelude::string::String;
+use sp_runtime::TryRuntimeError;
+
+/// Logs the error message and returns "Sanity test error"
+pub fn log_and_return_error_message(error_message: String) -> TryRuntimeError {
+	log::error!("{}", error_message);
+	TryRuntimeError::Other("Test")
+}
