1958d2317d9d45fd5b8071ae0ce4d58a0d580729
Shree Vatsa N <vatsa@kadalu.tech>
2024-02-06 17:19:52 +0530

    tests/chain-space: Add test for InvalidIdentifierLength
    Signed-off-by: Shree Vatsa N <vatsa@dhiway.com>

--- pallets/chain-space/src/tests.rs
+++ pallets/chain-space/src/tests.rs
@@ -4,6 +4,7 @@ use codec::Encode;
 use cord_utilities::mock::{mock_origin::DoubleOrigin, SubjectId};
 use frame_support::{assert_err, assert_ok, error::BadOrigin};
 use frame_system::RawOrigin;
+use identifier::IdentifierError;
 use sp_runtime::{traits::Hash, AccountId32};
 use sp_std::prelude::*;
 
@@ -427,6 +428,30 @@ fn creating_a_duplicate_space_should_fail() {
 		assert_err!(
 			Space::create(DoubleOrigin(author.clone(), creator.clone()).into(), space_digest,),
 			Error::<Test>::SpaceAlreadyAnchored
+		);
+	});
+}
+
+// TODO: Ideally this should be inside primitives/identifier/tests.rs
+// This test should fail when Identifier Length is below minimum length i.e 2,
+// Or if Identifier Length is above maximum length i.e 49.
+#[test]
+fn creating_a_invalid_identifier_length_should_fail() {
+	let space1 = [2u8; 1].to_vec();
+	let space2 = [2u8; 50].to_vec();
+	let space3 = [2u8; 40].to_vec();
+
+	new_test_ext().execute_with(|| {
+		assert_err!(
+			Ss58Identifier::create_identifier(&(space1).encode()[..], IdentifierType::Space),
+			IdentifierError::InvalidIdentifierLength
+		);
+		assert_err!(
+			Ss58Identifier::create_identifier(&(space2).encode()[..], IdentifierType::Space),
+			IdentifierError::InvalidIdentifierLength
+		);
+		assert_ok!(
+			Ss58Identifier::create_identifier(&(space3).encode()[..], IdentifierType::Space)
 		);
 	});
 }
