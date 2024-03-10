5eb5acaf2a1366ef5962fb5b73b37db317bac510
Shashank Kumar <shashank.kumar.phe22@itbhu.ac.in>
2024-03-11 05:16:59 +0530

    added tests

--- pallets/statement/src/tests.rs
+++ pallets/statement/src/tests.rs
@@ -1024,3 +1024,69 @@ fn updating_a_registered_statement_again_should_fail() {
 		);
 	});
 }
+
+#[test]
+fn registering_with_invalid_identifier_length_should_fail() {
+	let creator = DID_00;
+	let author = ACCOUNT_00;
+	let capacity = 5u64;
+	let statement = [77u8; 32];
+	let statement_digest: StatementDigestOf<Test> =
+		<Test as frame_system::Config>::Hashing::hash(&statement[..]);
+	let new_statement = [88u8; 32];
+	let new_statement_digest = <Test as frame_system::Config>::Hashing::hash(&new_statement[..]);
+
+	let raw_space = [2u8; 256].to_vec();
+	let space_digest = <Test as frame_system::Config>::Hashing::hash(&raw_space.encode()[..]);
+	let space_id_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&space_digest.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+	let space_id: SpaceIdOf = generate_space_id::<Test>(&space_id_digest);
+
+	let raw_schema = [11u8; 256].to_vec();
+	let schema: InputSchemaOf<Test> = BoundedVec::try_from(raw_schema)
+		.expect("Test Schema should fit into the expected input length of for the test runtime.");
+	let schema_id_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&schema.encode()[..], &space_id.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+	let schema_id: SchemaIdOf = generate_schema_id::<Test>(&schema_id_digest);
+
+	let auth_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&space_id.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+	let authorization_id: Ss58Identifier = generate_authorization_id::<Test>(&auth_digest);
+
+	let statement_id_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&statement_digest.encode()[..], &space_id.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+
+	let statement_id: StatementIdOf = generate_statement_id::<Test>(&statement_id_digest);
+
+	new_test_ext().execute_with(|| {
+		assert_ok!(Space::create(
+			DoubleOrigin(author.clone(), creator.clone()).into(),
+			space_digest,
+		));
+
+		assert_ok!(Space::approve(RawOrigin::Root.into(), space_id, capacity));
+
+		assert_ok!(Schema::create(
+			DoubleOrigin(author.clone(), creator.clone()).into(),
+			schema.clone(),
+			authorization_id.clone()
+		));
+
+		let too_short_data = [0u8; 82];
+		assert_err!(
+			Ss58Identifier::create_identifier(&too_short_data, IdentifierType::Statement),
+			identifier::IdentifierError::InvalidIdentifierLength
+		);
+
+		assert_ok!(Statement::register(
+			DoubleOrigin(author, creator).into(),
+			statement_digest,
+			authorization_id,
+			Some(schema_id)
+		));
+	});
+}
