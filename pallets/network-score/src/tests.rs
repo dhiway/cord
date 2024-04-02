22779771361526d78786a1b32c033760dbaef817
Zeel Darji <darjizeel587@gmail.com>
2024-04-02 06:55:08 +0530

    Rating Identifier already exists tests (#392)

--- pallets/network-score/src/tests.rs
+++ pallets/network-score/src/tests.rs
@@ -245,3 +245,70 @@ fn revise_rating_with_entry_entity_mismatch_should_fail() {
 		);
 	});
 }
+
+#[test]
+fn test_register_rating_id_already_exists() {
+	// Define test parameters
+	let creator = DID_00.clone();
+	let author = ACCOUNT_00.clone();
+	let message_id = BoundedVec::try_from([72u8; 10].to_vec()).unwrap();
+	let entity_uid = BoundedVec::try_from([73u8; 10].to_vec()).unwrap();
+	let provider_uid = BoundedVec::try_from([74u8; 10].to_vec()).unwrap();
+	let entry = RatingInputEntryOf::<Test> {
+		entity_uid,
+		provider_uid,
+		total_encoded_rating: 250u64,
+		count_of_txn: 7u64,
+		entity_type: EntityTypeOf::Logistic,
+		rating_type: RatingTypeOf::Overall,
+		provider_did: creator.clone(),
+	};
+	let entry_digest =
+		<Test as frame_system::Config>::Hashing::hash(&[&entry.encode()[..]].concat()[..]);
+	let raw_space = [2u8; 256].to_vec();
+	let space_digest = <Test as frame_system::Config>::Hashing::hash(&raw_space.encode()[..]);
+	let space_id_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&space_digest.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+	let space_id: SpaceIdOf = generate_space_id::<Test>(&space_id_digest);
+	let auth_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&space_id.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+	let authorization_id: AuthorizationIdOf =
+		Ss58Identifier::create_identifier(&auth_digest.encode()[..], IdentifierType::Authorization)
+			.unwrap();
+
+	new_test_ext().execute_with(|| {
+		System::set_block_number(1);
+
+		// Create a space
+		assert_ok!(Space::create(
+			DoubleOrigin(author.clone(), creator.clone()).into(),
+			space_digest
+		));
+		assert_ok!(Space::approve(RawOrigin::Root.into(), space_id, 3u64));
+
+		// Register the rating entry once
+		assert_ok!(Score::register_rating(
+			DoubleOrigin(author.clone(), creator.clone()).into(),
+			entry.clone(),
+			entry_digest,
+			message_id.clone(),
+			authorization_id.clone(),
+		));
+
+		// Remove message_id and provider_did from entries
+		<MessageIdentifiers<Test>>::remove(message_id.clone(), creator.clone());
+		// Attempt to register another rating entry with the same identifier
+		assert_err!(
+			Score::register_rating(
+				DoubleOrigin(author.clone(), creator.clone()).into(),
+				entry.clone(),
+				entry_digest,
+				message_id.clone(),
+				authorization_id.clone(),
+			),
+			Error::<Test>::RatingIdentifierAlreadyAdded
+		);
+	});
+}
