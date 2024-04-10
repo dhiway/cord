4f81e99712956f880a76b6a7151595a573c330a0
Jhanvi <jhanvi.ug20@nsut.ac.in>
2024-04-10 10:03:14 +0530

    [C4GT] Network-Authorization: Added test for InvalidNodeIdentifier (#410)

--- pallets/node-authorization/src/tests.rs
+++ pallets/node-authorization/src/tests.rs
@@ -63,6 +63,14 @@ fn add_well_known_node_works() {
 			),
 			Error::<Test>::NodeIdTooLong
 		);
+		assert_noop!(
+			NodeAuthorization::add_well_known_node(
+				RuntimeOrigin::signed(1),
+				test_node(TEST_NODE_7),
+				15
+			),
+			Error::<Test>::InvalidNodeIdentifier
+		);
 		assert_ok!(NodeAuthorization::add_well_known_node(
 			RuntimeOrigin::signed(1),
 			test_node(TEST_NODE_4),
@@ -113,6 +121,13 @@ fn remove_well_known_node_works() {
 		assert_noop!(
 			NodeAuthorization::remove_well_known_node(
 				RuntimeOrigin::signed(1),
+				test_node(TEST_NODE_7),
+			),
+			Error::<Test>::InvalidNodeIdentifier
+		);
+		assert_noop!(
+			NodeAuthorization::remove_well_known_node(
+				RuntimeOrigin::signed(1),
 				test_node(TEST_NODE_5)
 			),
 			Error::<Test>::NotExist
@@ -157,6 +172,14 @@ fn swap_well_known_node_works() {
 				test_node(TEST_NODE_5)
 			),
 			Error::<Test>::NodeIdTooLong
+		);
+		assert_noop!(
+			NodeAuthorization::swap_well_known_node(
+				RuntimeOrigin::signed(1),
+				test_node(TEST_NODE_7),
+				test_node(TEST_NODE_6)
+			),
+			Error::<Test>::InvalidNodeIdentifier
 		);
 		assert_noop!(
 			NodeAuthorization::swap_well_known_node(
