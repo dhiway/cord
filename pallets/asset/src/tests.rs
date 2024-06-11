a22476ba99765a92ac9b56f0a7bce4d127a57fd8
surajatreya <surajatreyac@gmail.com>
2024-06-11 17:06:02 +0530

    asset: Added test case for vc over issuance limit

--- pallets/asset/src/tests.rs
+++ pallets/asset/src/tests.rs
@@ -952,5 +952,107 @@ fn asset_over_issuance_should_not_succeed() {
 			Error::<Test>::OverIssuanceLimit
 		);
 	});
+}
 
+#[test]
+fn asset_vc_status_change_should_not_succeed() {
+	let creator = DID_00;
+	let author = ACCOUNT_00;
+
+	let capacity = 5u64;
+
+	let raw_space = [2u8; 256].to_vec();
+	let space_digest = <Test as frame_system::Config>::Hashing::hash(&raw_space.encode()[..]);
+	let space_id_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&space_digest.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+	let space_id: SpaceIdOf = generate_space_id::<Test>(&space_id_digest);
+
+	let auth_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&space_id.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+
+	let authorization_id: Ss58Identifier = generate_authorization_id::<Test>(&auth_digest);
+
+	let asset_desc = BoundedVec::try_from([72u8; 10].to_vec()).unwrap();
+	let asset_tag = BoundedVec::try_from([72u8; 10].to_vec()).unwrap();
+	let asset_meta = BoundedVec::try_from([72u8; 10].to_vec()).unwrap();
+	let asset_qty = 10;
+	let asset_value = 10;
+	let asset_type = AssetTypeOf::MF;
+
+	let entry = AssetInputEntryOf::<Test> {
+		asset_desc,
+		asset_qty,
+		asset_type,
+		asset_value,
+		asset_tag,
+		asset_meta,
+	};
+
+	let digest = <Test as frame_system::Config>::Hashing::hash(&[&entry.encode()[..]].concat()[..]);
+
+	let issue_id_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&digest.encode()[..], &space_id.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+
+	let asset_id: Ss58Identifier = generate_asset_id::<Test>(&issue_id_digest);
+
+	let issue_entry = AssetIssuanceEntryOf::<Test> {
+		asset_id: asset_id.clone(),
+		asset_owner: creator.clone(),
+		asset_issuance_qty: Some(20),
+	};
+
+	let issue_entry_digest =
+		<Test as frame_system::Config>::Hashing::hash(&[&issue_entry.encode()[..]].concat()[..]);
+
+	let instance_id_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[
+			&asset_id.encode()[..],
+			&creator.encode()[..],
+			&space_id.encode()[..],
+			&creator.encode()[..],
+			&issue_entry_digest.encode()[..],
+		]
+			.concat()[..],
+	);
+
+	let instance_id = generate_asset_instance_id::<Test>(&instance_id_digest);
+
+	new_test_ext().execute_with(|| {
+		assert_ok!(Space::create(
+			DoubleOrigin(author.clone(), creator.clone()).into(),
+			space_digest,
+		));
+
+		assert_ok!(Space::approve(RawOrigin::Root.into(), space_id, capacity));
+
+		assert_ok!(Asset::vc_create(
+			DoubleOrigin(author.clone(), creator.clone()).into(),
+			asset_qty,
+			digest,
+			authorization_id.clone()
+		));
+
+		assert_err!(
+			Asset::vc_issue(
+				DoubleOrigin(author.clone(), creator.clone()).into(),
+				issue_entry.clone(),
+				issue_entry_digest,
+				authorization_id
+			),
+			Error::<Test>::OverIssuanceLimit
+		);
+
+		assert_err!(
+			Asset::vc_status_change(
+				DoubleOrigin(author.clone(), creator.clone()).into(),
+				asset_id.clone(),
+				Some(instance_id.clone()),
+				AssetStatusOf::INACTIVE
+			),
+			Error::<Test>::AssetInstanceNotFound
+		);
+	});
 }
