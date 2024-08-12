d9944df506532e4f4b23e558e7099d22f7817029
Satish Mohan <54302767+smohan-dw@users.noreply.github.com>
2024-08-12 13:12:14 +0530

    runtime epoch updates

--- braid/runtimes/base/constants/src/lib.rs
+++ braid/runtimes/base/constants/src/lib.rs
@@ -47,7 +47,7 @@ pub mod time {
 	// NOTE: Currently it is not possible to change the slot duration after the chain has started.
 	//       Attempting to do so will brick block production.
 	pub const SLOT_DURATION: Moment = MILLISECS_PER_BLOCK;
-	pub const EPOCH_DURATION_IN_SLOTS: BlockNumber = prod_or_fast!(30 * MINUTES, MINUTES);
+	pub const EPOCH_DURATION_IN_SLOTS: BlockNumber = prod_or_fast!(HOURS, 2 * MINUTES);
 
 	// These time units are defined in number of blocks.
 	pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
