8572772243daf9056dfc0ad323bda012693b9021
Shree Vatsa N <vatsa@dhiway.com>
2025-03-19 10:49:27 +0530

    runtimes: Set spec version to 9600 (#595)
    Signed-off-by: Shreevatsa N <vatsa@dhiway.com>

--- runtimes/weave/src/lib.rs
+++ runtimes/weave/src/lib.rs
@@ -121,7 +121,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("weave"),
 	impl_name: create_runtime_str!("dhiway-cord"),
 	authoring_version: 0,
-	spec_version: 9500,
+	spec_version: 9600,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
