1dc70eb045a21463b87b5396523cd27a8346b496
Adi Bhagavath <adibhagawath@gmail.com>
2025-04-03 14:16:28 +0530

    Add a test for namespace update
    Signed-off-by: Adi Bhagavath <adibhagawath@gmail.com>

--- pallets/namespace/src/tests.rs
+++ pallets/namespace/src/tests.rs
@@ -60,3 +60,51 @@ fn add_delegate_should_succeed() {
 		));
 	});
 }
+
+//TEST FUNCTION FOR UPDATE
+#[test]
+fn update_namespace_should_succeed() {
+	let creator = ACCOUNT_00;
+
+	let raw_blob = [1u8; 256].to_vec();
+	let blob: NameSpaceBlobOf<Test> = BoundedVec::try_from(raw_blob)
+		.expect("Test blob should fit into the expected input length of for the test runtime.");
+
+	let namespace = [2u8; 256].to_vec();
+	let namespace_digest = <Test as frame_system::Config>::Hashing::hash(&namespace.encode()[..]);
+
+	let id_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&namespace_digest.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+	let namespace_id: NameSpaceIdOf = generate_namespace_id::<Test>(&id_digest);
+
+	let auth_id_digest = <Test as frame_system::Config>::Hashing::hash(
+		&[&namespace_id.encode()[..], &creator.encode()[..], &creator.encode()[..]].concat()[..],
+	);
+	let authorization_id: AuthorizationIdOf = generate_authorization_id::<Test>(&auth_id_digest);
+
+	new_test_ext().execute_with(|| {
+		assert_ok!(NameSpace::create(
+			frame_system::RawOrigin::Signed(creator.clone()).into(),
+			namespace_digest,
+			Some(blob.clone()),
+		));
+
+		assert!(
+			NameSpaces::<Test>::contains_key(namespace_id.clone()),
+			"Namespace was not stored!"
+		);
+
+		let namespace_updated = [3u8; 256].to_vec();
+		let namespace_digest_updated =
+			<Test as frame_system::Config>::Hashing::hash(&namespace_updated.encode()[..]);
+
+		assert_ok!(NameSpace::update(
+			frame_system::RawOrigin::Signed(creator.clone()).into(),
+			namespace_id,
+			namespace_digest_updated,
+			None,
+			authorization_id
+		));
+	});
+}
