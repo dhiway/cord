14be8f5a0d92881b1f3ea730bbdf05651c037ec4
Shreevatsa N <vatsa@dhiway.com>
2025-05-06 16:27:24 +0530

    pallet-identifier: Fix benchmarks and tests for new update
    Signed-off-by: Shreevatsa N <vatsa@dhiway.com>

--- pallets/identifier/src/tests.rs
+++ pallets/identifier/src/tests.rs
@@ -69,16 +69,18 @@ fn resolve_pallet_name_negative() {
 	});
 }
 
+/* TODO: This test is commented out because the set_network_id function is not present in
+ * identifier pallet */
 /// Test that setting and then retrieving the network id works correctly.
-#[test]
-fn set_and_get_network_id() {
-	new_test_ext().execute_with(|| {
-		let network_id = NetworkId::from(42u32);
-		Pallet::<Test>::set_network_id(network_id);
-		let retrieved = Pallet::<Test>::get_network_id();
-		assert_eq!(retrieved, network_id);
-	});
-}
+// #[test]
+// fn set_and_get_network_id() {
+// 	new_test_ext().execute_with(|| {
+// 		let network_id = NetworkId::from(42u32);
+// 		Pallet::<Test>::set_network_id(network_id);
+// 		let retrieved = Pallet::<Test>::get_network_id();
+// 		assert_eq!(retrieved, network_id);
+// 	});
+// }
 
 #[test]
 fn record_activity_positive() {
@@ -90,7 +92,7 @@ fn record_activity_positive() {
 
 		let event: EventTypeOf =
 			vec![1u8; 10].try_into().expect("Should create a valid bounded vector");
-		let seal = TimePoint { height: 1, index: 0 };
+		let seal = EventBlock { height: 1, index: 0 };
 
 		assert_ok!(Pallet::<Test>::state_event(&identifier, digest, event.clone(), seal.clone()));
 
