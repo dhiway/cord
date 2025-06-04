37d257a9a978dd1fce0096470d8ec9dc80a91ea9
Shree Vatsa N <vatsa@dhiway.com>
2025-06-04 12:07:23 +0530

    Update licensing of files (#641)
    Signed-off-by: Shreevatsa N <vatsa@dhiway.com>

--- runtimes/weave/src/identifier_api.rs
+++ runtimes/weave/src/identifier_api.rs
@@ -1,6 +1,6 @@
 // This file is part of CORD – https://cord.network
 
-// Copyright (C) 2019-2023 BOTLabs GmbH.
+
 // Copyright (C) Dhiway Networks Pvt. Ltd.
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Adapted to meet the requirements of the CORD project.
